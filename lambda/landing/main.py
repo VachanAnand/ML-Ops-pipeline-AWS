99d1652ea1d3c167ee2e095e926c3f8488db6753
Vachan Anand <vachan.anand@senexenergy.com.au>
2022-03-14 22:58:37 +1000

    Added lambda function template

---
+++ lambda/landing/main.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
