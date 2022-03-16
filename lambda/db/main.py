20b01ec0643cf1c812f24c497396b22b9fc199f0
Vachan Anand <vachan.anand@senexenergy.com.au>
2022-03-16 22:45:26 +1000

    Updated lambda function

--- lambda/db/main.py
+++ lambda/db/main.py
@@ -1,8 +1,22 @@
 import json
+import pip
+
+def save_data_dynamodb(bucket,key):
+    s3_uri = f's3://{bucket}/{key}'
+    print('s3_uri',s3_uri)
+    df = pd.read(s3_uri)
+    print(df)
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    for event_record in event['Records']:
+        body = json.loads(event_record['body'])
+        for record in json.loads(body['Message'])['Records']:
+            bucket = record['s3']['bucket']['name']
+            key = record['s3']['object']['key']
+            save_data_dynamodb(bucket,key)    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
